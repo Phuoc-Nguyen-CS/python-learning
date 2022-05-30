6c0f278eb0d28e241f6c95ee829b6d3962bb2aca
Phuoc-Nguyen-CS <phuocnguyen102800@gmail.com>
2022-05-30 13:31:36 -0700

    5/30/22 01:32 PM Created the screen and animated the snake moving TODO: 	Arrow Key Input movements 	Snake Growth through collecting "Food" 	Collision Detection

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,11 +1,34 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
 screen.bgcolor("black")
 screen.title("Snake")
 
+# Creating the three initial segments for the snake
 
-screen.exitonclick()
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+
+for positions in starting_positions:
+    snake_segment = Turtle()
+    snake_segment.pendown()
+    snake_segment.color("white")
+    snake_segment.shape("square")
+    snake_segment.penup()
+    snake_segment.goto(positions)
+    segments.append(snake_segment)
+
+screen.tracer(0, 0)
+
+game_start = True
+while game_start:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
+
 
 
